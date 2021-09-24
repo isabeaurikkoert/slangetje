8b7e2710025264890b0901334413b5c3d7d651d1
Isabeau Rikkoert <57255022+isabeaurikkoert@users.noreply.github.com>
2021-09-24 09:36:25 +0000

    poging 1: vierkant met pijltjes besturen

--- script.js
+++ script.js
@@ -7,3 +7,48 @@ class Snake{
     this.vx = vx;
     this.vy = vy;
   }
+
+  drawSnake(){	
+
+    square(this.x, this.y, 50, 50);
+   
+    //this.x+this.vx;
+   
+    //this.y+this.vy;
+   // if(this.x<=0 || this.x>500){
+   // this.vx=this.vx*-1;
+   // }
+  //  if(this.y<=0 || this.y>400){
+  //  this.vy=this.vy*-1;
+   // }
+  }
+  
+
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    this.y = this.y - 10;
+  } else if (keyCode === DOWN_ARROW) {
+   this.y = this.y + 10;
+  }
+  if (keyCode === LEFT_ARROW) {
+    this.x = this.x - 5;
+  } else if (keyCode === RIGHT_ARROW) {
+    this.x = this.x + 5;
+  }
+  
+}
+
+
+function setup() {
+	createCanvas(500, 400);
+
+  snake = new Snake(30,30,30,30,6,6);
+}
+
+function draw() {
+	background(225);
+	snake.drawSnake();
+
+}
