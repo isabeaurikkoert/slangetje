c47e52425b6a764eb06c3f5bd9c8c2ba7745622a
Isabeau Rikkoert <57255022+isabeaurikkoert@users.noreply.github.com>
2021-10-04 12:09:35 +0000

    collision x

--- script.js
+++ script.js
@@ -6,27 +6,24 @@ class Snake {
     this.h = h
     this.vx = vx;
     this.vy = vy;
-    this.total = 0;
-    this.tail = [];
+    this.c = "white";
+  }
 
-    this.eat = function(pos){
-      var d = dist(this.x, this.y, pos.x, pos.y);
-      if (d < 1) {
-        total++;
-        return true;
-      } else {
-        return false;
-      }
-    }
-   }
-  
-  drawSnake(){
+  drawSnake() {
 
-    circle(this.x, this.y, 10, 10);
-    fill(0,216,0);
+    fill(this.c);
+    rect(this.x, this.y, 10, 10);    
 
     this.x = this.x + this.vx;
     this.y = this.y + this.vy;
+    
+
+    if (this.x + 10 > eten.x && this.x < eten.x + 10) {
+      this.c = "red";
+    }
+    else{
+      this.c = "white";
+    }
 
   }
 
@@ -41,78 +38,37 @@ var eten;
 function setup() {
   createCanvas(500, 300);
   snake = new Snake(30, 30, 30, 30, 2, 0);
-  eten = pickLocation();
-  framerate = 10;
+  eten = createVector(random(width), random(height));
 }
-function pickLocation() {
-var cols = floor(width/300);
-var rows = floor(height/500);
-eten = creatVector(floor(random(cols)), floor(random(rows)));
-//food.mult(scl);
-}
-
-eat(pos){
-  let x = this.body[this.body.length-1].x
-  let y = this.body[this.body.length-1].y
-  if( x == pos.x && y == pos.y) {
-    this.grow();
-    return true;
-  }
-  return false;
-}
-
-
 
 function draw() {
   background(img1);
   snake.drawSnake();
-  
-  if (snake.eat(eten)) {
-    eten = pickLocation();
-  }
 
-
-  fill(255,0,100);
+  fill(255, 0, 100);
   rect(eten.x, eten.y, 10, 10);
 
-  
+
 }
 
 function keyPressed() {
   console.log(keyCode);
 
-  if(keyCode == 40){
+  if (keyCode == 40) {
     snake.vx = 0;
     snake.vy = 2;
   }
-  if(keyCode == 37){
+  if (keyCode == 37) {
     snake.vx = -2;
     snake.vy = 0;
   }
-  if(keyCode == 38){
+  if (keyCode == 38) {
     snake.vx = 0;
     snake.vy = -2;
   }
-  if(keyCode == 39){
+  if (keyCode == 39) {
     snake.vx = 2;
     snake.vy = 0;
   }
 }
 
-//this.dir = function(x, y) {
-  this.xspeed = x;
-  this.yspeed = y;
- }
-
-//this.update = function() {
-  for (var i = 0; i < total; i++) {
-  }
-
-  this.x = this.x + this.xspeed*scl;
-  this.y = this.y + this.yspeed*scl;
-
-  this.x = constrain(this.x, 0, width-scl);
-  this.y = constrain(this.y, 0, width-scl);
-
-
-  }
